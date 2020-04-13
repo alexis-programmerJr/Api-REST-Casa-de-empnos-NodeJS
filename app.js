9331b2618e7709d9e19ea0a7999d2f9b3c6bb6d6
alexis-programmerJr <daniel.santos@gcabsa.com.mx>
2020-04-12 18:41:40 -0700

    Funcionalidad base de funcionalidad RESD
    La base de res con la con un modelo POST

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv/config');
+//Middlewares
+app.use(cors())
+app.use(bodyParser.json());
+
+//ROUTES
+app.get('/',(req,res)=> {
+    res.send('we are on home');
+});
+//Import rputers
+const postsRoute = require('./routes/posts');
+app.use('/posts',postsRoute);
+//Connect to DB
+mongoose.connect(
+process.env.DB_CONNECTION,
+{useNewUrlParser:true},
+()=>{console.log('connected to db');
+});
+
+//How to we start listening to the server
+app.listen(3000);
---
+++ routes/posts.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/post');
+
+router.get('/',(req,res)=> {
+    try{
+        const posts = Post.find();
+        res.json(posts)
+    }catch(err){
+        res.json({message:err})
+    }
+});
+//SUBMITS A POST
+router.post('/', async (req,res)=> {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+    post.save()
+    .then(data=>{
+        res.json(data);
+    }).catch(err =>{
+       res.json({message:err}) 
+    });
+});
+
+//SPECIFIC POST
+router.get('/:postId',async (req,res)=>{
+    try{
+        const post = Post.findById(req.params.postId);
+        res.json(post);
+    }catch(err){
+        res.json({message: err})
+    }
+});
+//Delete Post
+router.delete('/:postId', async (req,res)=>{
+    try{
+        const removePost = Post.remove({_id: req.params.postId});
+        res.json(removePost);
+    }catch(err){
+        res.json({message: err});
+    }
+
+});
+
+//Update a post 
+router.patch('/postId', async (req,res)=>{
+    try{
+        const updatepost = await Post.updateOne(
+            {_id:req.params.postId}
+            ,{$set:{title:req.body.title}}
+        );
+        res.json(updatepost);
+    }catch(err){
+        res.json({message: err});
+    }
+})
+
+module.exports = router;
